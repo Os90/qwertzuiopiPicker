6f8717cf36de21132d31c512a5a3aa8dc3211cf5
osman <osman_2008@hotmail.de>
2017-11-05 19:40:46 +0100

    Initial Commit

---
+++ iPicker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iPicker
+//
+//  Created by Osman Ashraf on 05.11.17.
+//  Copyright © 2017 Osman Ashraf. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
